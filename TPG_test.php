a02846d8552f1190594d4b6fbb866e64e555963f
Rőbert Szarvas <rob.szarvas@gmail.com>
2017-10-04 18:08:10 +1100

    Adding function for task 1 - printing asterixes

--- TPG_test.php
+++ TPG_test.php
@@ -1,6 +1,28 @@
 <?pHp
 
 /**
+ * Task 1
+ *
+ * Printing asterixes
+ *
+ * @note using '<br/>' as linebreak because I assume the output goes to a browser, other breakers might be necessary if this is not the case, maybe as input parameters
+ */
+function Task1(){
+	for ( $i = 1; $i <= 10; $i++ ){
+		if ( $i <= 5){
+			$k = $i;
+		}else{
+			$k = 10 - $i + 1;
+		}
+		for ( $j = 1; $j <= $k; $j++ ){
+			print "*";
+		}
+		
+		print "<br/>";
+	}
+}
+
+/**
  * Task 4
  *
  * Converting arabic numbers to roman numerals
